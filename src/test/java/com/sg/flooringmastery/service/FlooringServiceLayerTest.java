e9bf3e5c91da1f61c50a7dec48208857d439b374
Theebika Thiyagaraja Iyer <theebika6@hotmail.com>
2025-03-10 11:48:52 -0400

    started unit testing and added UML and flowchart

---
+++ src/test/java/com/sg/flooringmastery/service/FlooringServiceLayerTest.java
@@ -0,0 +1,50 @@
+package com.sg.flooringmastery.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlooringServiceLayerTest {
+
+    @org.junit.jupiter.api.Test
+    void getProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStateTax() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStateTaxes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllOrders() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void editOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNextOrderNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllOrderDates() {
+    }
+}
